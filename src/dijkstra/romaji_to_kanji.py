1fa5f419a97737be9d4cd64f131b1953f0f45e24
tkpy543 <takspooh@gmail.com>
2022-04-15 15:48:54 +0000

    docstringの追加

--- src/dijkstra/romaji_to_kanji.py
+++ src/dijkstra/romaji_to_kanji.py
@@ -4,6 +4,14 @@ from src.types.type_dijkstra import EkimeiT
 
 
 def romaji_to_kanji(eki: str, lst: List[EkimeiT]) -> str:
+    """ローマ字の駅名をもとに漢字駅名を取得する
+
+    Args:
+        eki (str): ローマ字の駅名
+        lst (List[EkimeiT]): 駅情報のリスト
+    Returns:
+        str: 対応する駅の漢字表記
+    """
     if len(lst) == 0:
         return ""
     first: EkimeiT = lst.pop(0)
